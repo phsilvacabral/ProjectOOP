1497ad0749db8289cccac4c6dca254d9bf58c749
Matheus Lowen <119804113+matheuslowen@users.noreply.github.com>
2024-05-22 08:37:23 -0300

    Add files via upload

---
+++ PJBL/Aereo.java
@@ -0,0 +1,19 @@
+package PJBL;
+
+public class Aereo extends Veiculo{
+    private String tipoPropulsao;
+
+    public Aereo(int codVeiculo, String tipo, String marca, String modelo, int ano, int quilometragem, String cor,
+                 int capacidadeDePassageiro, double preco, int quantidade, String tipoPropulsao) {
+        super(codVeiculo, tipo, marca, modelo, ano, quilometragem, cor, capacidadeDePassageiro, preco, quantidade);
+        this.tipoPropulsao = tipoPropulsao;
+    }
+
+    public String getTipoPropulsao() {
+        return tipoPropulsao;
+    }
+
+    public void setTipoPropulsao(String tipoPropulsao) {
+        this.tipoPropulsao = tipoPropulsao;
+    }
+}
