2290e0b93f4dbc6da2efba20a0e31411514efc86
Matheus Lowen <119804113+matheuslowen@users.noreply.github.com>
2024-05-22 08:36:59 -0300

    Add files via upload

--- PJBL/Aquatico.java
+++ PJBL/Aquatico.java
@@ -1,4 +1,19 @@
 package PJBL;
 
-public class Aquatico {
+public class Aquatico extends Veiculo{
+    private String tipoProplsao;
+
+    public Aquatico(int codVeiculo, String tipo, String marca, String modelo, int ano, int quilometragem, String cor,
+                    int capacidadeDePassageiro, double preco, int quantidade, String tipoProplsao) {
+        super(codVeiculo, tipo, marca, modelo, ano, quilometragem, cor, capacidadeDePassageiro, preco, quantidade);
+        this.tipoProplsao = tipoProplsao;
+    }
+
+    public String getTipoProplsao() {
+        return tipoProplsao;
+    }
+
+    public void setTipoProplsao(String tipoProplsao) {
+        this.tipoProplsao = tipoProplsao;
+    }
 }
