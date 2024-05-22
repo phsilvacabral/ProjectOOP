f9fe1690b143ae7314d573c2aa729a7c93dd052a
Matheus Lowen <119804113+matheuslowen@users.noreply.github.com>
2024-05-22 08:37:47 -0300

    Add files via upload

--- PJBL/Administrador.java
+++ PJBL/Administrador.java
@@ -1,19 +1,40 @@
 package PJBL;
-
-public class Administrador extends Usuario{
-    private String historicoAcoesAdministrativa; //Talvez tenha que ser ArrayList
-
-    public Administrador(int idUsuario, String nome, String tipo, String cpf, String email, String senha,
-                         String telefone, String endereco, String historicoAcoesAdministrativa) {
-        super(idUsuario, nome, tipo, cpf, email, senha, telefone, endereco);
-        this.historicoAcoesAdministrativa = historicoAcoesAdministrativa;
+import java.util.List;
+import java.util.Scanner;
+public class Administrador extends Usuario {
+    public Administrador(int idUsuario, String nome, String cpf, String email, String senha, String telefone,
+                         String endereco) {
+        super(idUsuario, nome, "Administrador", cpf, email, senha, telefone, endereco);
     }
 
-    public String getHistoricoAcoesAdministrativa() {
-        return historicoAcoesAdministrativa;
-    }
+    //Método do Administrador para criar Usuario
+    public void criarNovoUsuario(Scanner scanner, List<Usuario> usuarios) throws LoginException {
+        System.out.print("Digite o id do novo usuário: ");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.print("Digite o nome do novo usuário: ");
+        String nome = scanner.nextLine();
+        System.out.print("Digite o CPF do novo usuário: ");
+        String cpf = scanner.nextLine();
+        System.out.print("Digite o email do novo usuário: ");
+        String email = scanner.nextLine();
+        System.out.print("Digite a senha do novo usuário: ");
+        String senha = scanner.nextLine();
+        System.out.print("Digite o telefone do novo usuário: ");
+        String telefone = scanner.nextLine();
+        System.out.print("Digite o endereço do novo usuário: ");
+        String endereco = scanner.nextLine();
+        System.out.print("Digite o tipo do novo usuário (Administrador/Funcionario): ");
+        String tipo = scanner.nextLine();
 
-    public void setHistoricoAcoesAdministrativa(String historicoAcoesAdministrativa) {
-        this.historicoAcoesAdministrativa = historicoAcoesAdministrativa;
+        Usuario novoUsuario;
+        if (tipo.equalsIgnoreCase("Administrador")) {
+            novoUsuario = new Administrador(id, nome, cpf, email, senha, telefone, endereco);
+        } else {
+            novoUsuario = new Funcionario(id, nome, cpf, email, senha, telefone, endereco);
+        }
+
+        usuarios.add(novoUsuario);
+        SistemaDeLogin.salvarUsuario(novoUsuario);
+        System.out.println("Novo usuário criado com sucesso.");
     }
 }
